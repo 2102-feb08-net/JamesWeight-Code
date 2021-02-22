6219428692dbbbc5deddcc89e468890661479ceb
JamesWeight <JamesWeight>
2021-02-22 11:45:59 -0800

    FacilitationForExercises

---
+++ Storage/RPScontrollerClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RPScontrollerNS
+{
+    class RPSlogic
+    {
+        static void RPSlogicCall(char _logicInput)
+        {
+            
+        }
+    }
+}
---
+++ Storage/RPSviewClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RPSviewNS
+{
+    class RPSinterface
+    {
+        static void RPSinterfaceDisplay(char _logicInput)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
